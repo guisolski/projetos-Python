4338da7f94550b2bc59ffa00217c84ce35cb5b4c
Guilherme Solski Alves <guigusolski@gmail.com>
2019-11-08 10:56:44 -0300

    Update chatBot.py

--- chatBot.py
+++ chatBot.py
@@ -1,5 +1,5 @@
 import math
-def arrayPalavras(text):
+def array_to_palavras(text):
     a = []
     for i in text:
         for j in i.split(" "):
@@ -8,21 +8,21 @@ def arrayPalavras(text):
     return a
 def df(documents,word):
   s = 0
-  for i in documents:
-      if word in i:
+  # -1 porque tem o q incluido
+  for i in range(len(documents)-1):
+      if word in documents[i]:
         s += 1
   return s
 def idf(documents,word):
-  #-2 porque tem o corpus incluido
-  t = len(documents)
+  #-1 porque tem o q incluido
+  t = len(documents)-1
   d = df(documents,word)
-  print(t)
-  print(d)
   return math.log10((t/d))
 
 def matriz_idf(documents,list_word):
   m = []
-  for i in range(len(documents)-1):
+  # não prisa do -1 pq agora estamos montando a matriz
+  for i in range(len(documents)):
     t = []
     for j in list_word:
       if j in documents[i]:
@@ -34,12 +34,35 @@ def matriz_idf(documents,list_word):
 def similaridade(matriz_idf,i):
     s = 0
     for j in range(len(matriz_idf[0])):
-        s += (matriz_idf[i][j] * matriz_idf[len(matriz_idf)-1][j])
+        if matriz_idf[i][j] != 0 and matriz_idf[len(matriz_idf)-1][j] != 0:
+                s += matriz_idf[i][j]*matriz_idf[len(matriz_idf)-1][j]
     return s
 
-texto = ["Shipment of gold damaged in a fire", "Delivery of silver arrived in a silver truck","Shipment of gold arrived in a truck"]
+def index_maior_valor(t):
+    m = t[0]
+    index = 0
+    for i in range(len(t)):
+        if t[i] > m:
+            index = i
+            m = t[i]
+    return index
 
-corpus = sorted(arrayPalavras(texto))
-#q = "gold silver truck"
-#texto.append(q)
-print(idf(texto,"truck"))
+def maior_simi(matriz_idf):
+    t = []
+    for i in range(len(matriz_idf)-1):
+        t.append(similaridade(matriz_idf,i))
+    return index_maior_valor(t)
+
+
+
+#resultados finais
+reposta = ['Parabéns você se encaixa no perfil de morados esse são os imoveis q recomendamos para você realizar a comprar: \nCasa de Madeira - Um quarto, Duas cozinhas']
+#cria casos de validação
+texto = ["Shipment of gold damaged in a fire", "Delivery of silver arrived in a silver truck","Shipment of gold arrived in a truck"]
+corpus = sorted(array_to_palavras(texto))
+#aqui vai ser onde montamos os slots e depois o q
+q = "gold silver truck"
+texto.append(q)
+#parte q calcula qual resposta ele chegará
+matriz = matriz_idf(texto,corpus)
+print(maior_simi(matriz))
