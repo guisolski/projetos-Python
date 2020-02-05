51a5a6bd1388fd64fae0f93f420a68f819ba8a26
Guilherme Solski Alves <guigusolski@gmail.com>
2020-02-05 16:20:16 -0300

    Update hangmanGame.py

--- arquivos/hangmanGame.py
+++ arquivos/hangmanGame.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -11,10 +12,11 @@ def setInPos(a,p,l):
     for i in p:
         a[i] = l
     return a
-
-word = "abcda"
+def randomWord(t):
+    lista_char = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    return "".join([lista_char[random.randint(0,len(lista_char))] for i in range(t)])
+word = randomWord(5)
 Input_word,end,erros = [None for i in range(len(word))],0,0
-
 while end < len(word) and erros < len(word):
     cls()
     printWord(Input_word)
